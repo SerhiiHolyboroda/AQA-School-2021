6a7bfc0381242330fd4c0623d8909914c5ad47d7
Serhii Holyboroda <49484600+SerhiiHolyboroda@users.noreply.github.com>
2021-01-30 18:37:57 +0200

    Adding first homework tasks 1 - 2

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -14,15 +14,18 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args)  throws IOException {
-        BufferedReader reader =
-                new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));
         String day = reader.readLine();
-//        task1
+//task1
+
       System.out.println(WeekDays.valueOf(day.toUpperCase()).getDay());
-//         task2
+
+//task2
+
         String name = reader.readLine();
 
-      //  String str = "Ivan Ivanov";
+//  String str = "Ivan Ivanov";
+
         System.out.println(name.matches("(([A-Z][a-z]+)\\s[A-Z][a-z]+)"));
     }
 }
